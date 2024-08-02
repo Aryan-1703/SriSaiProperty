dafaca52f0a34168bed57fa45aeeccf383fcb680
Akshil Patel <116317952+Akshil23@users.noreply.github.com>
2024-08-02 01:13:50 -0400

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render
+(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus facilis aperiam corporis velit ullam reiciendis illum, eveniet adipisci quas dolorum, fugit in. Dolor ex tempore porro laudantium quod. Unde, maxime.<h1>
