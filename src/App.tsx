242bfc083f0eddd015f86ce0bda47610781ac932
Aryan <aryan.talpada@crosslinxtransit.ca>
2024-08-03 14:33:07 -0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import WelcomeSection from './components/WelcomeSection';
-import Footer from './components/Footer';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import WelcomeSection from "./components/WelcomeSection";
+import Upcoming from "./components/Upcoming";
+import Footer from "./components/Footer";
+import "./App.css";
 
 const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <HeroSection />
-      <WelcomeSection />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<HeroSection />
+			<WelcomeSection />
+			<Upcoming />
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
---
+++ src/components/Upcoming.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Upcoming.css";
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Upcoming = () => {
+	return (
+		<Container fluid className="upcoming-section text-center">
+			Future Projects
+		</Container>
+	);
+};
+
+export default Upcoming;
