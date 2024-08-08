6cdff94c14a3d1d34b1d377e9d9582aaca4150ce
Aryan <aryan17032@gmail.com>
2024-08-08 11:12:48 -0400

    Organize repo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,14 @@
-import Header from "../components/Header";
-import HeroSection from "../components/HeroSection";
-import WelcomeSection from "../components/WelcomeSection";
-import UpcomingProjects from "../components/UpcomingProject";
-import Footer from "../components/Footer";
+import Header from "../components/Home/Header";
+import HeroSection from "../components/Home/HeroSection";
+import WelcomeSection from "../components/Home/WelcomeSection";
+import UpcomingProjects from "../components/Home/UpcomingProject";
+import Footer from "../components/Home/Footer";
 
 
 const Home = () => {
     return (
         <div className="Home">
+            
             <HeroSection />
             <WelcomeSection />
             <UpcomingProjects />
