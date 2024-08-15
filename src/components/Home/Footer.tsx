bef0ec79b4178582d0dd51e9a459fbbd90f2cfc1
Akshil23 <akshilp05@gmail.com>
2024-08-15 01:47:58 -0400

    Footer Updated

--- src/components/Home/Footer.tsx
+++ src/components/Home/Footer.tsx
@@ -3,6 +3,18 @@ import '../../styles/Home/Footer.css';
 const Footer = () => {
   return (
     <footer>
+
+      <p>Website Developed by Akshil Patel and Aryan Talpada.</p>
+      
+      <p className="contact-info">
+        <a href="mailto:Akshilp05@gmail.com?subject=Contact%20from%20Website&body=Hi%20Akshil%20Pate,%0A%0AI%20would%20like%20to%20get%20in%20touch%20with%20you%20regarding%20...">
+          akshilp05@gmail.com
+        </a>
+        <span> | </span>
+        <a href="mailto:aryan17032@gmail.com?subject=Contact%20from%20Website&body=Hi%20Aryan%20Talpada,%0A%0AI%20would%20like%20to%20get%20in%20touch%20with%20you%20regarding%20...">
+          aryan17032@gmail.com
+        </a>
+      </p>
       <p>&copy; CMS Builds 2024. All rights reserved.</p>
     </footer>
   );
